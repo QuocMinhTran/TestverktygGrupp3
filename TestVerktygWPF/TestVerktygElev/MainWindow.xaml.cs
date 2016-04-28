026c63150a7abc99b62f2a01c17733ff6d2336e8
Sirvan Minbary <moot4king@hotmail.com>
2016-04-28 13:53:27 +0200

    created layout for mainWindow

--- TestVerktygWPF/TestVerktygElev/MainWindow.xaml.cs
+++ TestVerktygWPF/TestVerktygElev/MainWindow.xaml.cs
@@ -25,5 +25,16 @@ namespace TestVerktygElev
             InitializeComponent();
             
         }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO: send UserName and Password to database and login if there is a match
+        }
+
+        private void TextBlock_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key.Equals(Key.Enter))
+                btnLogin_Click(sender, e);
+        }
     }
 }
