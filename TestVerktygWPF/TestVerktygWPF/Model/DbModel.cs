0ce8be4a0afb4978f3310ebb135546a3d76494ad
Wikberg <snokjonte@hotmail.com>
2016-04-28 14:07:32 +0200

    Syncat

--- TestVerktygWPF/TestVerktygWPF/Model/DbModel.cs
+++ TestVerktygWPF/TestVerktygWPF/Model/DbModel.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 
