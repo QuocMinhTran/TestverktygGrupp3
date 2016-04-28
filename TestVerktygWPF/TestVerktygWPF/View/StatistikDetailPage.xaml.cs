76f11cec57793e0c4b8c8615bbef42e9e9b309b3
Minh Tran <quocminh2303@gmail.com>
2016-04-28 11:04:21 +0200

    new WPF admin pages

---
+++ TestVerktygWPF/TestVerktygWPF/View/StatistikDetailPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestVerktygWPF.View
+{
+    /// <summary>
+    /// Interaction logic for StatistikDetailPage.xaml
+    /// </summary>
+    public partial class StatistikDetailPage : Page
+    {
+        public StatistikDetailPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
