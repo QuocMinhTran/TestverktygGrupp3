8a91c1aa8e331f7c8c5809d6b438679dff1970d0
Minh Tran <quocminh2303@gmail.com>
2016-04-28 13:52:43 +0200

    classes and views

---
+++ TestVerktygWPF/TestVerktygElev/View/ElevTestPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestVerktygElev.View
+{
+    /// <summary>
+    /// Interaction logic for ElevTestPage.xaml
+    /// </summary>
+    public partial class ElevTestPage : Page
+    {
+        public ElevTestPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
